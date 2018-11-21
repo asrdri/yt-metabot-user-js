2c3bbe28889073529ba695df0a4c255263213f0f
asrdri <36112208+asrdri@users.noreply.github.com>
2018-11-21 13:05:27 +0300

    layout hotfix
    * [newdesign] fix for updated youtube layout
    * [reports] new http-based system enabled for testing

--- yt-metabot.user.js
+++ yt-metabot.user.js
@@ -2,7 +2,7 @@
 // @name         MetaBot for YouTube
 // @namespace    yt-metabot-user-js
 // @description  More information about users and videos on YouTube.
-// @version      181016
+// @version      181121
 // @homepageURL  https://vk.com/public159378864
 // @supportURL   https://github.com/asrdri/yt-metabot-user-js/issues
 // @updateURL    https://raw.githubusercontent.com/asrdri/yt-metabot-user-js/master/yt-metabot.meta.js
@@ -107,6 +107,7 @@ const regexdatemob = /joined_date_text(.*?)"}, {"text": "(.*?)"}]/;
 const regexid = /"video_id":"(.*?)"/;
 const regexlinew = /"logged_in","value":"(.*?)"/;
 const regexliold = /"logged_in":"(.*?)"/;
+const regexun = /"user_display_name":"(.*?)"/;
 const regexlang = /"host_language":"(.*?)"/;
 const regexlangmob = /\\"host_language\\": \\"(.*?)\\"/;
 const regexannyto = /(.*)(\r\n|\n\r|\n)([\W\w]+)/;
@@ -114,7 +115,13 @@ const ERKYurl = 'https://raw.githubusercontent.com/YTObserver/YT-ACC-DB/master/m
 const annYTOurl = 'https://raw.githubusercontent.com/YTObserver/YT-ACC-DB/master/announcement.txt';
 const minDCTime = 36*61;
 const maxDCTime = 71*58;
-const alertURL = 'https://кремлеботы.рф/alert';
+const alerturl = 'https://кремлеботы.рф/alert';
+const reporturl = 'http://h134224.s23.test-hf.su/0xJvXVyCI252wZL.php';
+const reportparv = 'tmcLmrBkistDeCj=';
+const reportparlc = '&pEcmTXm4aFmtGPj=';
+const reportparbid = '&MzpVezaWBKy4HB4=';
+const reportparbn = '&0gsgbZk9RCqiaCp=';
+const reportparun = '&SVpoJEzFPFwm9SM=';
 var alertSent = false;
 var annYTOtxt = [];
 var arrayERKY = [];
@@ -316,7 +323,7 @@ function spinnercheckNew() {
           setTimeout(recheckallNew, 2000);
         } else {
           $('div#main.style-scope.ytd-comment-renderer').each(function() {
-            var cNode = $(this).find("#published-time-text")[0];
+            var cNode = $(this).find(".published-time-text")[0];
             deleteitemNew(this, $(cNode).find("a")[0].href);
           });
         }
@@ -800,7 +807,7 @@ function sendAlert(jNode) {
   if (alertSent || GM_config.get('option5') === false)
     return;
   if (ytmode === 1) {
-    var commentURL = $(jNode).find('#published-time-text a').prop('href');
+    var commentURL = $(jNode).find('.published-time-text a').prop('href');
   } else {
     var commentURL = $(jNode).find('.comment-renderer-time a').prop('href');
   }
@@ -808,7 +815,7 @@ function sendAlert(jNode) {
   var videoid = getURLParameter('v', location.search);
   var data = $.param({v: videoid, lc: commendid});
   var request = new XMLHttpRequest();
-  request.open("POST", alertURL, true);
+  request.open("POST", alerturl, true);
   request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
   request.send(data);
   alertSent = true;
@@ -882,14 +889,13 @@ function parseitem(jNode) {
     newspan.innerHTML = '<img id="checkbtn" src="' + checkb + '" title="Проверить дату регистрации" style="cursor: help" />';
     newspan.id = 'checksp';
     pNode.insertBefore(newspan, pNode.firstChild);
-    t30span.innerHTML += '\u2003<span id="sendlink" style="cursor: pointer" title="Помогите пополнить список известных ботов - отправьте нам ссылку на подозрительный комментарий">Сообщить</span>';
+    t30span.innerHTML += '\u2003<span id="sendlinkoff" style="cursor: pointer; text-decoration: line-through; display: none" title="Спасибо, данный пользователь будет проверен">Сообщить</span><span id="sendlink" style="cursor: pointer" title="Помогите пополнить список известных ботов - отправьте нам данные о подозрительном комментарии">Сообщить</span>';
     $(comURL).after(t30span);
     $(jNode).find("img")[0].addEventListener("click", function checkcomment() {
       checkdate(pNode);
     }, false);
     $(jNode).find("#sendlink")[0].addEventListener("click", function displayinfo() {
-      sendinfo();
-      $(jNode).find("#sendlink").css("text-decoration", "line-through");
+      sendinfo($(jNode).find("#t30sp"), $(comURL).find("a")[0].href, userID, $(jNode).parent().parent().find("img")[0].alt, regexliold);
     }, false);
   }
   if (GM_config.get('option4') === true) {
@@ -988,7 +994,7 @@ function parseitemNew(jNode) {
       foundIDc3 = arrayListC3.indexOf(userID);
     }
   }
-  var comURL = $(jNode).find("#published-time-text")[0];
+  var comURL = $(jNode).find(".published-time-text")[0];
   var t30span = document.createElement('span');
   t30span.innerHTML = '\u2003<span id="about" style="cursor: pointer; ' + iconstyledef + '" title="Открыть страницу с датой регистрации">\u2753</span>\u2003<span id="top30" style="cursor: pointer" title="Найти другие комментарии автора с помощью агрегатора ТОП30"><font color="#7777fa">top</font><font color="#fa7777">30</font></span>' + spanlistpadd;
   t30span.id = 't30sp';
@@ -1021,14 +1027,13 @@ function parseitemNew(jNode) {
     newspan.innerHTML = '<img id="checkbtn" src="' + checkb + '" title="Проверить дату регистрации" style="cursor: help" />';
     $(newspan).attr('data-chan', $(jNode).find("a#author-text")[0].href);
     pNode.insertBefore(newspan, pNode.firstChild);
-    t30span.innerHTML += '\u2003<span id="sendlink" style="cursor: pointer" title="Помогите пополнить список известных ботов - отправьте нам ссылку на подозрительный комментарий">СООБЩИТЬ</span>';
+    t30span.innerHTML += '\u2003<span id="sendlinkoff" style="cursor: pointer; text-decoration: line-through; display: none" title="Спасибо, данный пользователь будет проверен">СООБЩИТЬ</span><span id="sendlink" style="cursor: pointer" title="Помогите пополнить список известных ботов - отправьте нам данные о подозрительном комментарии">СООБЩИТЬ</span>';
     $(comURL).append(t30span);
     $(jNode).find("#checkbtn")[0].addEventListener("click", function checkcommentNew() {
       checkdateNew($(pNode).parent());
     }, false);
     $(jNode).find("#sendlink")[0].addEventListener("click", function displayinfoNew() {
-      sendinfo();
-      $(jNode).find("#sendlink").css("text-decoration", "line-through");
+      sendinfo($(jNode).find("#t30sp"), $(comURL).find("a")[0].href, userID, $(jNode).parent().find("img#img")[0].alt, regexlinew);
     }, false);
   }
   if (GM_config.get('option4') === true) {
@@ -1101,8 +1106,59 @@ function deleteitemNew(jNode, url) {
   }
 }
 
-function sendinfo() {
-  window.open('tg://join?invite=Fei-E00mcIZy_15xqaE4aQ');
+function sendinfo(jNode, comlink, botid, botname, regexpid) {
+  if(regexpid.exec(document.body.innerHTML)[1] == "1") {
+    var urlv = getURLParameter('v', comlink);
+    var urllc = getURLParameter('lc', comlink);
+    var txtun = regexun.exec(document.body.innerHTML)[1];
+    var params = reportparv + urlv + 
+      reportparlc + urllc + 
+      reportparbid + botid + 
+      reportparbn + window.btoa(unescape(encodeURIComponent(botname))) + 
+      reportparun + window.btoa(unescape(encodeURIComponent(txtun)));
+    console.log('params ' + params);
+    var answer = confirm('Ссылка на комментарий: ' + comlink +
+      '\nID профиля автора: ' + botid +
+      '\nИмя профиля автора: ' + botname +
+      '\nИмя вашего профиля (для защиты от спама): ' + txtun +
+      '\n\nДанные будут отправлены по протоколу HTTP без шифрования.\nПродолжить?');
+    if (answer) {
+      $(jNode).find("#sendlink").html('');
+      if (typeof GM_xmlhttpRequest !== 'undefined') {
+        GM_xmlhttpRequest({
+          method: "POST",
+          url: reporturl,
+          data: params,
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+          },
+          onload: function(response) {
+            console.log("[MetaBot for Youtube] Report sent for user ID " + botid + ". Code " + response.status);
+            $(jNode).find("#sendlink").hide();
+            $(jNode).find("#sendlinkoff").show();
+          }
+        });
+      } else if (typeof GM !== 'undefined') {
+        GM.xmlHttpRequest({
+          method: "POST",
+          url: reporturl,
+          data: params,
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+          },
+          onload: function(response) {
+            console.log("[MetaBot for Youtube] Report sent for user ID " + botid + ". Code " + response.status);
+            $(jNode).find("#sendlink").hide();
+            $(jNode).find("#sendlinkoff").show();
+          }
+        });
+      } else {
+        console.log("[MetaBot for Youtube] Unable to get supported cross-origin XMLHttpRequest function.");
+      }
+    }
+  } else {
+    alert("Необходимо войти в аккаунт Google.");
+  }
 }
 
 function listpadd(jNode, response, url) {
